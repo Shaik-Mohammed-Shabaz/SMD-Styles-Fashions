b6705429a59cd027a90504d14ef151ed40820b98
SHAIK MOHAMMED SHABAZ <mohammedshabaz143786@gmail.com>
2026-08-02 13:56:34 +0530

    Added back to top button

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -991,3 +991,34 @@ if(menuToggle && navMenu){
     });
 
 }
+
+// ==========================
+// Back To Top
+// ==========================
+
+const backToTop = document.getElementById("backToTop");
+
+if(backToTop){
+
+    window.addEventListener("scroll",()=>{
+
+        backToTop.style.display =
+        window.scrollY > 300
+        ? "block"
+        : "none";
+
+    });
+
+    backToTop.addEventListener("click",()=>{
+
+        window.scrollTo({
+
+            top:0,
+
+            behavior:"smooth"
+
+        });
+
+    });
+
+}
